4a9f2b2b8512068df3fe410a0ad5a866bd1eb77f
Kaushik-Shahare <kaushikshahare0@gmail.com>
2024-06-19 09:07:39 +0530

    Added signin and signup controller

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# mr-loggage-web
-this is offical developer project team work from mr loggage
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,12 +5,72 @@ dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY;
 const User = require("../models/User");
 
+const saltRounds = 10;
+
 const signup = async (req, res) => {
-  res.send("Signup route is working");
+  const { email, password, fullName, address, phoneNumber, gender } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      fullName,
+      address,
+      phoneNumber,
+      gender,
+    });
+
+    await newUser.save();
+
+    const token = jwt.sign({ id: newUser._id }, SECRET_KEY);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.status(201).json({ message: "User successfully registered" });
+  } catch (error) {
+    console.log("Signup error: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 const signin = async (req, res) => {
-  res.send("Signin route is working");
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "No user found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, SECRET_KEY);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({ message: "User successfully signed in" });
+  } catch (error) {
+    console.log("Signin error: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 module.exports = { signup, signin };

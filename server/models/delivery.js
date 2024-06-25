2ccb721de7cd11fc7792f5131ac44084c5e135ef
VishankWagh <vishankswagh@gmail.com>
2024-06-25 11:56:39 +0530

    added the store delivery details API and updated the schema

--- server/models/delivery.js
+++ server/models/delivery.js
@@ -1,77 +1,69 @@
 const mongoose = require("mongoose");
 
-const addressSchema = new mongoose.Schema({
-  societyAddress: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  state: {
-    type: String,
-    required: true,
-  },
-  pincode: {
-    type: String,
-    required: true,
-  },
-});
-
 const deliverySchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
-  itemName: {
-    type: String,
-    required: true,
-  },
-  smallquantity: {
+  smallQuantity: {
     type: Number,
     required: true,
     min: 0,
   },
-  mediumquantity: {
+  mediumQuantity: {
     type: Number,
     required: true,
     min: 0,
   },
-  largequantity: {
+  largeQuantity: {
     type: Number,
     required: true,
     min: 0,
   },
-  deliveryAddress :{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "addressSchema",
-    required: true,
+  pickupAddress: {
+    societyAddress: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    pincode: {
+      type: String,
+      required: true,
+    },
   },
-  trainNumber: {
-    type: String,
-    required: true,
-  },
-  PNR: {
-    type: String,
-    required: true,
-  },
-  coachNumber: {
-    type: String,
-    required: true,
-  },
-  seatNumber: {
-    type: String,
-    required: true,
-  },
-  arrivalTime: {
-    type: Date,
-    required: true,
-  },
-  departureTime: {
-    type: Date,
-    required: true,
+  deliveryAddress: {
+    trainNumber: {
+      type: String,
+      required: true,
+    },
+    PNR: {
+      type: String,
+      required: true,
+    },
+    coachNumber: {
+      type: String,
+      required: true,
+    },
+    seatNumber: {
+      type: String,
+      required: true,
+    },
+    arrivalTime: {
+      type: Date,
+      required: true,
+    },
+    departureTime: {
+      type: Date,
+      required: true,
+    }
   },
   deliveryStatus: {
     type: String,
@@ -79,9 +71,16 @@ const deliverySchema = new mongoose.Schema({
     enum: ["Pending", "In Transit", "Delivered", "Cancelled"],
     default: "Pending",
   },
-  expectedDeliveryDate: {
-    type: Date,
-  },
+  pickUpSlot: {
+    date: {
+      type: Date,
+      required: true,
+    },
+    slot: {
+      type: String,
+      required: true,
+    },
+  }
 }, { timestamps: true });
 
 const Delivery = mongoose.model("Delivery", deliverySchema);

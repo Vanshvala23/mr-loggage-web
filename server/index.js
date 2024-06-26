b58ec48f333ef9eb1925fc996787a65ac8df92a8
VanshVala23 <vanshvala01@gmail.com>
2024-06-26 19:44:29 +0530

    "Edited"

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const profileRouter = require("./routes/profileRoutes");
 const testimonialRouter = require("./routes/testimonialRoutes");
 const deliveryRouter = require("./routes/deliveryRoutes");
 
-// mongoose.connect("mongodb+srv://mrloggage.r4ejgme.mongodb.net/", {
-// useNewUrlParser: true,
-// useUnifiedTopology: true,
-// })
 
 mongoose.connect("mongodb+srv://MrLoggage:mrloggagekartik@mrloggage.r4ejgme.mongodb.net",{
   useNewUrlParser: true,
